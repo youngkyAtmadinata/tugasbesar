f9731b88df61014743ec128339d515a21b0db15f
Diva Adittiya Putra <38122028+divap@users.noreply.github.com>
2018-06-07 21:42:25 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/sistemsewa/';
+$config['base_url'] = 'http://localhost/sistemsewa2/';
 
 /*
 |--------------------------------------------------------------------------
