0dab20ade02637397dabd3ceb198af02b1a3613e
Diva Adittiya Putra <38122028+divap@users.noreply.github.com>
2018-06-07 21:36:39 +0700

    Update routes.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,7 +50,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'web';
-$route['tentangsaya'] = 'about';
-$route['berita/(:num)'] = 'news/index/$1';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
