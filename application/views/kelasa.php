acf8b8bf75a4917dec6f7776ce9675b051c8906e
Diva Adittiya Putra <38122028+divap@users.noreply.github.com>
2018-06-07 22:13:37 +0700

    Add files via upload

---
+++ application/views/kelasa.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+
+<html lang="">
+
+<head>
+<title>TugasAkhir</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<body id="top">
+
+<div class="wrapper row1">
+  <header id="header" class="hoc clear"> 
+
+ 
+    <div id="logo">
+	 <center>
+	<img src="images/demo/apartment1.png"></a>
+      <h1><a href="index.html">Sewa Apartemen Malang</a>
+	  </h1>
+	  </center>
+    </div>
+    
+
+  </header>
+</div>
+
+<div class="wrapper row2">
+  <nav id="mainav" class="hoc clear"> 
+
+    <ul class="clear">
+      <li class="active"><a href="index.html">BERANDA</a></li>
+	    <li></li>
+		
+      <li><a class="drop" href="#">GALERI</a>
+        <ul>
+		  <li><a href="pages/sidebar-left.html"></a></li>
+          <li><a href="pages/gallery.html"></a></li>
+          <li><a href="pages/full-width.html"></a></li>
+        </ul>
+      </li>
+        <ul>
+          <li><a href="#">Level 2</a></li>
+          <li><a class="drop" href="#">Level 2 + Drop</a>
+            <ul>
+              <li><a href="#"></a></li>
+              <li><a href="#"></a></li>
+              <li><a href="#"></a></li>
+            </ul>
+          </li>
+          <li><a href="#">Level 2</a></li>
+        </ul>
+      </li>
+	   <li></li>
+	   <li><a href="login/login.php">LOGIN ADMIN</a></li>
+
+
+  </nav>
+</div>
+
+<div class="wrapper bgded overlay" style="background-image:url('images/demo/backgrounds/apartemen.jpg');">
+  <div id="pageintro" class="hoc clear"> 
+
+    <article>
+      <div>
+        <p class="heading"></p>
+        <h2 class="heading"></h2>
+        
+		
+		
+      </div>
+      <footer>
+        
+      </footer>
+    </article>
+
+  </div>
+</div>
+
+
+<div class="wrapper row3">
+  <section class="hoc container clear"> 
+
+    <div class="sectiontitle">
+      <h6 class="heading">Keterangan</h6>
+      <p>Dibedakan Menjadi beberapa kelas</p>
+    </div>
+    <div class="group">
+      <article class="one_third first"><a href="kelasa.php"><img class="btmspace-30" src="images/demo/01.jpg" alt=""></a>
+        <h6 class="nospace heading">Kelas A</h6>
+        <ul class="nospace meta">
+         
+         
+        </ul>
+        <p>Di kamar ini terdapat&hellip;</p>
+        <footer class="nospace"><a class="btn" href="kelasa.php">Read More</a></footer>
+      </article>
+      
+    </div>
+
+  </section>
+</div>
+
+<div class="wrapper bgded overlay light" style="background-image:url('images/demo/backgrounds/04.jpg');">
+  <article class="hoc cta clear"> 
+
+    <h6 class="three_quarter first">Tertarik Menyewa Kamar Di Apartemen Kami ? </h6>
+    <footer class="one_quarter"><a class="btn" href="#">Pesan Sekarang &raquo;</a></footer>
+
+  </article>
+</div>
+
+<div class="wrapper row4">
+  <footer id="footer" class="hoc clear"> 
+
+    <div class="one_quarter first">
+      <h6 class="heading">Kontak Kami</h6>
+      <ul class="nospace btmspace-30 linklist contact">
+        <li><i class="fa fa-map-marker"></i>
+          <address>
+          Jalan Baiduri Pandan &amp; 30, Malang, 65144
+          </address>
+        </li>
+        <li><i class="fa fa-phone"></i> +62-812-9887-1221</li>
+        <li><i class="fa fa-envelope-o"></i> infoapartemen@gmail.com</li>
+      </ul>
+      <ul class="faico clear">
+        <li><a class="faicon-facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+        <li><a class="faicon-twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+        <li><a class="faicon-dribble" href="#"><i class="fa fa-dribbble"></i></a></li>
+        <li><a class="faicon-linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
+      </ul>
+    </div>
+    <div class="one_quarter">
+      <h6 class="heading"></h6>
+      <ul class="nospace linklist">
+        
+      </ul>
+    </div>
+    <div class="one_quarter">
+      <h6 class="heading"></h6>
+      <ul class="nospace linklist">
+        
+      </ul>
+    </div>
+    <div class="one_quarter">
+      <h6 class="heading">Jika Anda memiliki pertanyaan lain</h6>
+      <p class="nospace btmspace-15">Silahkan kirim ke email kami</p>
+      <form method="post" action="#">
+        <fieldset>
+          <legend>Newsletter:</legend>
+          <input class="btmspace-15" type="text" value="" placeholder="Name">
+          <input class="btmspace-15" type="text" value="" placeholder="Email">
+          <button type="submit" value="submit">Kirim</button>
+        </fieldset>
+      </form>
+    </div>
+
+  </footer>
+</div>
+
+<div class="wrapper row5">
+  <div id="copyright" class="hoc clear"> 
+	
+	<center>
+	<p>
+    Diva Adittiya Putra / 1641720190</p>
+	</center>
+	
+  </div>
+</div>
+
+<a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
+
+<script src="layout/scripts/jquery.min.js"></script>
+<script src="layout/scripts/jquery.backtotop.js"></script>
+<script src="layout/scripts/jquery.mobilemenu.js"></script>
+</body>
+</html>
+
+
+
+
+
+<img src="<?php echo base_url(); ?>assets/image/apartment1.png">
